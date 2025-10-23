0360f15d7a7d1ca16e53da0700ac537a8ed278cc
tania-mixich <mixich.taniaa@gmail.com>
2025-10-23 13:59:38 +0300

    Ride DTO and entity diagram

---
+++ backend/Carma.Application/DTOs/Ride/RideCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using Carma.Domain.Enums;
+using Carma.Domain.ValueObjects;
+
+namespace Carma.Application.DTOs.Ride;
+
+public record RideCreateDto(
+    DateTime CreatedAt,
+    DateTime UpdatedAt,
+    Location PickupLocation,
+    Location DropOffLocation,
+    DateTime PickupTime,
+    double Price,
+    int AvailableSeats,
+    Status Status
+    );
---
+++ backend/Carma.Application/Mappers/RideMapper.cs
@@ -0,0 +1,38 @@
+﻿using Carma.Application.DTOs.Ride;
+using Carma.Domain.Entities;
+
+namespace Carma.Application.Mappers;
+
+public static class RideMapper
+{
+    public static Ride MapToRide(RideCreateDto rideCreateDto, User organizer)
+    {
+        return new Ride
+        {
+            CreatedAt = rideCreateDto.CreatedAt,
+            UpdatedAt = rideCreateDto.UpdatedAt,
+            PickupLocation = rideCreateDto.PickupLocation,
+            DropOffLocation = rideCreateDto.DropOffLocation,
+            PickupTime = rideCreateDto.PickupTime,
+            Price = rideCreateDto.Price,
+            AvailableSeats = rideCreateDto.AvailableSeats,
+            Status = rideCreateDto.Status,
+            Organizer = organizer
+        };
+    }
+
+    public static RideGetDto MapToRideGetDto(Ride ride)
+    {
+        return new RideGetDto
+        (
+            ride.CreatedAt,
+            ride.UpdatedAt,
+            ride.PickupLocation,
+            ride.DropOffLocation,
+            ride.PickupTime,
+            ride.Price,
+            ride.AvailableSeats,
+            ride.Status
+        );
+    }
+}
